c11a03b64d94dd29b39c0ac8c9c7a19df86f2b3e
IvanMatias1618 <tacolopez1618033@outloo.com>
2025-07-24 11:33:08 -0600

    mas poemas

---
+++ Hola.md
@@ -0,0 +1,1 @@
+>Hola;\n >Queria decirte que siempre estaras en mi memoria.\n>Quiero plasmar aqui un poco de esa memoria que hay en mi: \n>De ti. \n>Y hacia ti.
---
+++ juego/src/main.js
@@ -0,0 +1,17 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 250,
+  height: 240,
+  BackgroundColor: "#f3f",
+  parent: 'game',
+  scene: {
+    preload,
+    create,
+    update,
+  }
+}
+
+function preload(){}
+
+function create(){}
+function update(){}
